3ddea6a8d6140affdb301005a3242554872826fb
Jordan Halfman <jordanhalfman@gmail.com>
2022-07-14 12:45:32 -0700

    commit tree dc2086c2bf8eed8e4ab921f4091b5250f5970a66

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,35 @@ function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
 
+  function onChangeType(e) {
+    setFilters({type: e.target.value});
+  }
+
+  function onFindPetsClick() {
+    if (filters.type === "all") {
+      fetch(`http://localhost:3001/pets`)
+      .then(resp=>resp.json())
+      .then(data => setPets(data))
+    }
+    else {
+      fetch(`http://localhost:3001/pets?type=${filters.type}`)
+      .then(resp=>resp.json())
+      .then(data => setPets(data))
+    }
+  }
+
+  function onAdoptPet(id) {
+    const newPets = pets.map(pet => {
+      if (pet.id === id) {
+        return {...pet, isAdopted: true};
+      }
+      else {
+        return pet;
+      }
+    })
+    setPets(newPets);
+  }
+
   return (
     <div className="ui container">
       <header>
@@ -15,10 +44,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onChangeType={onChangeType} onFindPetsClick={onFindPetsClick}/>
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={onAdoptPet}/>
           </div>
         </div>
       </div>
